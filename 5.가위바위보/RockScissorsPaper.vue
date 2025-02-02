a3bfca2488ab47a176a7dac0147ec9759454a70a
a2175 <as217512@gmail.com>
2025-02-02 14:54:15 +0900

    5-1. vue-devtools와 기타 정보들

---
+++ 5.가위바위보/RockScissorsPaper.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    data() {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
