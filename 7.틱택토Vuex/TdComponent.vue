e2743e0d7b8b8c27925edbb833735c6d7d540131
a2175 <as217512@gmail.com>
2025-02-21 15:32:39 +0900

    7-7. Vuex Mutations

--- 7.틱택토Vuex/TdComponent.vue
+++ 7.틱택토Vuex/TdComponent.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-  import EventBus from './EventBus';
+  import { CLICK_CELL, SET_WINNER, RESET_GAME, CHANGE_TURN, NO_WINNER } from './store';
 
   export default {
     props: {
@@ -15,7 +15,42 @@
       onClickTd() {
         if (this.cellData) return;
 
-        EventBus.$emit('clickTd', this.rowIndex, this.cellIndex);
+        this.$store.commit(CLICK_CELL, { row: this.rowIndex, cell: this.cellIndex });
+
+        // 가로, 세로, 대각선이 삼목이 됐는지 체크
+        let win = false;
+        if (this.tableData[this.rowIndex][0] === this.turn && this.tableData[this.rowIndex][1] === this.turn && this.tableData[this.rowIndex][2] === this.turn) {
+          win = true;
+        }
+        if (this.tableData[0][this.cellIndex] === this.turn && this.tableData[1][this.cellIndex] === this.turn && this.tableData[2][this.cellIndex] === this.turn) {
+          win = true;
+        }
+        if (this.tableData[0][0] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][2] === this.turn) {
+          win = true;
+        }
+        if (this.tableData[0][2] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][0] === this.turn) {
+          win = true;
+        }
+
+        if (win) { // 이긴 경우: 3줄 달성
+          this.$store.commit(SET_WINNER, this.turn);
+          this.$store.commit(RESET_GAME);
+        } else { // 무승부
+          let all = true; // all이 true면 무승부라는 뜻
+          this.tableData.forEach((row) => { // 무승부 검사
+            row.forEach((cell) => {
+              if (!cell) {
+                all = false;
+              }
+            });
+          });
+          if (all) { // 무승부
+            this.$store.commit(NO_WINNER);
+            this.$store.commit(RESET_GAME);
+          } else {
+            this.$store.commit(CHANGE_TURN);
+          }
+        }
       }
     }
   }
--- 7.틱택토Vuex/store.js
+++ 7.틱택토Vuex/store.js
@@ -1,14 +1,46 @@
+import Vue from 'vue';
 import Vuex from 'vuex';
+
+export const SET_WINNER = 'SET_WINNER'; // 사용예시: import { SET_WINNER, CLICK_CELL, CHANGE_TURN } from './store';
+export const CLICK_CELL = 'CLICK_CELL';
+export const CHANGE_TURN = 'CHANGE_TURN';
+export const RESET_GAME = 'RESET_GAME';
+export const NO_WINNER = 'NO_WINNER';
 
 export default new Vuex.Store({
   state: {
-  
+    tableData: [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ],
+    turn: 'O',
+    winner: '',
   }, // vue의 data와 비슷
   getters: {
   
   }, // vue의 computed와 비슷
   mutations: {
-
+    [SET_WINNER](state, winner) {
+      state.winner = winner;
+    },
+    [CLICK_CELL](state, { row, cell }) {
+      Vue.set(state.tableData[row], cell, state.turn);
+    },
+    [CHANGE_TURN](state) {
+      state.turn = state.turn === 'O' ? 'X' : 'O';
+    },
+    [RESET_GAME](state) {
+      state.turn = 'O';
+      state.tableData = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ];
+    },
+    [NO_WINNER](state) {
+      state.winner = '';
+    }
   }, // state를 수정할 때 사용해요. 동기적으로
   actions: {
   
