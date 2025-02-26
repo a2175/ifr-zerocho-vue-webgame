b59eba1193300cb7ff7b15af9c476c78439963a9
a2175 <as217512@gmail.com>
2025-02-26 14:21:12 +0900

    8-1. 지뢰찾기 구조 만들기

---
+++ 8.지뢰찾기/MineSweeper.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <mine-form />
+    <div>{{timer}}</div>
+    <table-component />
+    <div>{{result}}</div>
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex';
+  import store from './store';
+  import MineForm from './MineForm';
+  import TableComponent from './TableComponent';
+  
+  export default {
+    store,
+    components: {
+      MineForm,
+      TableComponent,
+    },
+    computed: {
+      ...mapState(['timer', 'result']),
+    },
+    methods: {
+    
+    },
+  };
+</script>
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
+</style>
