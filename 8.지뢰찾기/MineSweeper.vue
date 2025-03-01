e472e8eabae2bd39b3fcbf2d27881ca708b43d29
a2175 <as217512@gmail.com>
2025-03-01 15:52:37 +0900

    8-4. 타이머 켜고 끄기

--- 8.지뢰찾기/MineSweeper.vue
+++ 8.지뢰찾기/MineSweeper.vue
@@ -9,10 +9,11 @@
 
 <script>
   import { mapState } from 'vuex';
-  import store from './store';
+  import store, { INCREMENT_TIMER } from './store';
   import MineForm from './MineForm';
   import TableComponent from './TableComponent';
   
+  let interval = null;
   export default {
     store,
     components: {
@@ -20,11 +21,22 @@
       TableComponent,
     },
     computed: {
-      ...mapState(['timer', 'result']),
+      ...mapState(['timer', 'result', 'halted']),
     },
     methods: {
     
     },
+    watch: {
+      halted(value, oldValue) {
+        if (value === false) { // false일 때 게임 시작
+          interval = setInterval(() => {
+            this.$store.commit(INCREMENT_TIMER);
+          }, 1000);
+        } else { // 게임 중단
+          clearInterval(interval);
+        }
+      },
+    }
   };
 </script>
 
